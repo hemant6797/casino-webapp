4deb503ef20aaeacde8edb05a2656806833e37af
Rahul-kumar-hub <rahulsinghss15121998@gmail.com>
2021-10-22 12:29:21 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Content from "./Content";
+import background from "./images/background1.jpg";
 
 function App() {
+  const [islogin, setLogin] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    price: "",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid container-xxl p-0">
+      <div
+        style={{
+          backgroundImage: `url(${background}) `,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          minHeight: "100vh",
+
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Navbar
+          islogin={islogin}
+          setLogin={setLogin}
+          setUser={setUser}
+          user={user}
+        />
+        <div style={{ minHeight: "70vh" }}>
+          <Content user={user} setUser={setUser} islogin={islogin} />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
